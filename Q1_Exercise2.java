26c736d72b52a9e1b357005cd2693f9cc9d1e429
PatrickMaximusTurno <112913272+PatrickMaximusTurno@users.noreply.github.com>
2022-09-08 11:28:52 +0800

    Add files via upload

---
+++ Q1_Exercise2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1_exercise2;
+
+/**
+ *
+ * @author MUON
+ */
+public class Q1_Exercise2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application l
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Welcome to Higher or Lower what would you like to do?: ");
+        String gameStart = sc.nextLine();
+        
+    }
+    
+}
