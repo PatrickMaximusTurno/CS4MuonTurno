0761f6b7d03862f09665bd8a4226c6d991e6a3fd
PatrickMaximusTurno <112913272+PatrickMaximusTurno@users.noreply.github.com>
2022-09-08 11:31:45 +0800

    Add files via upload

---
+++ Q1_Exercise1.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1_exercise1;
+
+/**
+ *
+ * @author MUON
+ */
+public class Q1_Exercise1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       String name = "Mostima";
+       String OpType = "AoE Caster";
+       int level = 1;
+       System.out.println("Operator 1 " + "Name: " + name + " Type: " + OpType 
+        + " Level: " + level);
+        
+       String name1 = "Lappland";
+       String OpType1 = "Ranged Guard";
+       int level1 = 70;
+       System.out.println("Operator 2 " + "Name: " + name1 + " Type: " + OpType1 
+       + " Level: " + level1);
+       
+       String name2 = "Exusiai";
+       String OpType2 = "Sharpshooter";
+       int level2 = 24;
+       System.out.println("Operator 3 " + "Name: " + name2 + " Type: " + OpType2 
+       + " Level: " + level2);
+       
+      System.out.println("Lappland and Mostima's levels are the same: " + (level
+      == level1 ) );
+       
+      System.out.println("Exusiai has a higher level than Mostima: " + (level <
+      level2) );
+      
+      System.out.println("Total Level of Operators: " + (level +
+      level1 + level2) );
+    }
+        
+
+}
