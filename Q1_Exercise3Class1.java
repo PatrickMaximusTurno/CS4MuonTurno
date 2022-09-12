2ea5902d32c9953c27edafe3a37da11ea98b40b2
PatrickMaximusTurno <112913272+PatrickMaximusTurno@users.noreply.github.com>
2022-09-12 08:59:39 +0800

    Add files via upload

---
+++ Q1_Exercise3Class1.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1_exercise3.pkgclass.pkg1;
+
+/**
+ *
+ * @author MUON
+ */
+public class Q1_Exercise3Class1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+ public class Singer {
+    String name;
+    int noOfPerformances;
+    double earnings;
+    Song favSong;   
+   public void performForAudience(){
+       //+1 to noOfPerformances and earnings = 100 x (audience #)
+   }
+ public void changeFavSong(){
+     //revisit this later
+        
+    }
+ }
+
---
+++ Q1_Exercise3Class2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package q1_exercise3.class2;
+
+/**
+ *
+ * @author MUON
+ */
+public class Q1_Exercise3Class2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+     public class Song {
+     //should more fields be added?
+     String name;
+     
+ }
+}
